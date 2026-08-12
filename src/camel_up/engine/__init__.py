42f3641584c897adf2c1293583127e60db088e28
Liang Wang <bigbigbang87@gmail.com>
2026-08-12 23:13:32 +1000

    refactor: add deterministic dice and atomic setup

--- src/camel_up/engine/__init__.py
+++ src/camel_up/engine/__init__.py
@@ -1,5 +1,12 @@
 """Stable public types and queries for the Camel Up engine."""
 
+from camel_up.engine.dice import (
+    DieRoll,
+    SetupRoll,
+    reset_leg_dice,
+    roll_die,
+    setup_game,
+)
 from camel_up.engine.state import (
     CAMEL_ORDER,
     DIE_ORDER,
@@ -21,9 +28,14 @@ __all__ = [
     "CamelId",
     "CamelPosition",
     "DieId",
+    "DieRoll",
     "GameState",
     "SpectatorTile",
+    "SetupRoll",
     "carried_camels",
     "position_of",
+    "reset_leg_dice",
+    "roll_die",
+    "setup_game",
     "stack_at",
 ]
